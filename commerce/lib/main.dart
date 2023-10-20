cdbb48110f19e1eeea27ddffd907c35d47832f2c
TranUy99 <tranuy0907@gmail.com>
2023-10-20 10:28:08 +0700

    create

---
+++ commerce/lib/main.dart
@@ -0,0 +1,33 @@
+
+import 'package:commmerce/src/features/welcome/view/welcome.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return 
+    // BlocProvider(
+      // create: (context) => SubjectBloc(),
+      // child: 
+      ScreenUtilInit(
+        builder: (context,child)=> MaterialApp(
+            title: 'Flutter Demo',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+            home: const Welcome()
+          ),
+      );
+  }
+}
